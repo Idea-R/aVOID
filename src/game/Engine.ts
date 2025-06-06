cae80ae7a88deb4049921116d69db784db34becc
MadXent <shane@ideas-realized.com>
2025-06-06 15:21:23 -0700

    Updated Engine.ts

--- src/game/Engine.ts
+++ src/game/Engine.ts
@@ -761,12 +761,3 @@ export default class Engine {
             shadowsEnabled: this.shadowsEnabled,
             maxParticles: this.dynamicMaxParticles,
             
-          }
-        }
-        )
-      }
-    }
-  }
-}
-
-export default Engine
